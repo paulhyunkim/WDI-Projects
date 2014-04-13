ccae646268e7af8fdc71823d51c0f76f4e077167
Paul Kim <paulhyunkim@gmail.com>
2014-04-13 16:52:56 -0700

    third commit

---
+++ 0411_RockPaperScissor/js/script.js
@@ -0,0 +1,45 @@
+window.onload = startup;
+
+function startup() {
+  var buttons = document.getElementsByClassName('button');
+  for (i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = play;
+  }
+}
+
+var ruleBook = ["rock", "paper", "scissor", "rock", "paper"];
+
+function play(event) {
+  console.log(event);
+  var playerChoice = this.id;
+  var compChoice = randGenerator();
+  whoWins(playerChoice, compChoice);
+}
+
+function whoWins(playerChoice, compChoice) {
+  var playerIndex = ruleBook.indexOf(playerChoice);
+  var ruleForHand = ruleBook.slice(playerIndex, playerIndex + 3);
+  var compIndex = ruleForHand.indexOf(compChoice);
+
+  document.getElementById('player').innerHTML = "You chose: " + playerChoice;
+  document.getElementById('comp').innerHTML = "Computer chose: " + compChoice;
+
+  if (compIndex == 0) {
+    document.getElementById('result').innerHTML="You Tie!";
+  } else if (compIndex == 1) {
+     document.getElementById('result').innerHTML="You Lose!";
+  } else if (compIndex == 2) {
+     document.getElementById('result').innerHTML="You Win!";
+  }
+}
+  
+function randGenerator() {
+  var randNum = Math.floor((Math.random()*3));
+  if (randNum == 0) {
+    return "rock";
+  } else if (randNum == 1) {
+    return "paper";
+  } else if (randNum == 2) {
+    return "scissor";
+  }
+}
